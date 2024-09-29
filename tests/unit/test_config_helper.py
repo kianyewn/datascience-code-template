667b9f3ed4b7c1369a149dc454f28466e4dc8a62
Kian Yew Ngieng <kianyewngieng@Kians-MacBook-Air.local>
2024-09-29 18:38:50 +0800

    remove imports and do proper formatting

--- tests/unit/test_config_helper.py
+++ tests/unit/test_config_helper.py
@@ -1,20 +1,19 @@
-from src.utils.config_helper import ConfigYAML
 import pytest
-import unittest.mock as mock
+from src.utils.config_helper import ConfigYAML
+
 
 @pytest.fixture
-def mydict():  
+def mydict():
     mydict = {"a": 1, "b": 2, "c": 3}
     return mydict
 
 
 def test_ConfigYAML(mocker, mydict):
-    YAML_PATH = 'data/test_config.yaml'
-    m1 = mocker.patch('src.utils.config_helper.ConfigYAML.save', return_value=True)
-    m2 = mocker.patch('src.utils.config_helper.ConfigYAML.delete', return_value=True)
+    YAML_PATH = "data/test_config.yaml"
+    m1 = mocker.patch("src.utils.config_helper.ConfigYAML.save", return_value=True)
+    m2 = mocker.patch("src.utils.config_helper.ConfigYAML.delete", return_value=True)
     _ = ConfigYAML.save(mydict, YAML_PATH)
     ConfigYAML.delete(YAML_PATH)
-    
+
     m1.assert_called_with(mydict, YAML_PATH)
     m2.assert_called_with(YAML_PATH)
-    
